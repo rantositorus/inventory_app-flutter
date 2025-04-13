e93a2ea8b5ab14301ed6ab86a93c63833677537f
rantositorus <rantositorus44@gmail.com>
2025-04-13 22:02:55 +0700

    feat: add update records in the Hive Local database

--- lib/inventory_page.dart
+++ lib/inventory_page.dart
@@ -3,20 +3,42 @@ import 'package:hive_ce_flutter/hive_flutter.dart';
 import 'package:inventory_app/hive_boxes.dart';
 import 'package:inventory_app/product.dart';
 
-class InventoryPage extends StatelessWidget{
+class InventoryPage extends StatefulWidget{
   const InventoryPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final products = Hive.box<Product>(productBox).values;
+  State<InventoryPage> createState() => _InventoryPageState();
+}
 
+class _InventoryPageState extends State<InventoryPage> {
+  void _increment(Box<Product> productBox, Product product) {
+    product.stock++;
+    productBox.put(product.key, product);
+  }
+
+  void _decrement(Box<Product> productBox, Product product) {
+    if (product.stock > 0) {
+      product.stock--;
+      productBox.put(product.key, product);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Inventory'),
       ),
-      body: products.isEmpty
-          ? const Center(child: Text('No products available'))
-          : ListView.builder(
+      body: ValueListenableBuilder<Box<Product>>(
+        valueListenable: Hive.box<Product>(productBox).listenable(),
+        builder: (BuildContext context, Box<Product> box, Widget? _) {
+          final products = box.values.toList();
+
+          if(products.isEmpty) {
+            return const Center(child: Text('No products available'));
+          }
+
+          return ListView.builder(
               itemCount: products.length,
               itemBuilder: (BuildContext context, int index) {
                 final product = products.toList()[index];
@@ -32,11 +54,13 @@ class InventoryPage extends StatelessWidget{
                     child: Row(
                       children: [
                         IconButton(
-                          onPressed: () {},
+                          onPressed: () => _increment(box, product),
                           icon: const Icon(Icons.add),
                         ),
                         IconButton(
-                          onPressed: () {},
+                          onPressed: product.stock == 0
+                            ? null
+                            : () => _decrement(box, product) ,
                           icon: const Icon(Icons.remove),
                         )
                       ],
@@ -44,7 +68,9 @@ class InventoryPage extends StatelessWidget{
                   ),  
                 );
               },
-          )
+          );
+        }
+      )
     );
   }
 }
